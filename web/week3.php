e7e44aae4d28c1b173dfb1f6578b61fe8afa2076
moksaam <mem16001@byui.edu>
2018-01-24 19:33:37 -0700

    updated my php file

--- web/week3.php
+++ web/week3.php
@@ -1,14 +1,11 @@
 <?php
-// Variables
+// Variables for our form
 $username = $email = $cs = $web = $cit = $ce = $comments = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
    $username = test_input($_POST["username"]);
    $email = test_input($_POST["email"]);
-   $cs = test_input($_POST["cs"]);
-   $web = test_input($_POST["web"]);
-   $cit = test_input($_POST["cit"]);
-   $ce = test_input($_POST["ce"]);
+   $major = test_input($_POST["major"]);
    $comments = test_input($_POST["comments"]);
 }
 
@@ -18,6 +15,26 @@ function test_input($data) {
    $data = htmlspecialchars($data);
   
    return $data;
+}
+
+// Check if the form has been submitted
+if (isset($_POST['submit']) {
+   
+   $username = $_REQUEST["username"];
+   
+   echo "<p>Username: </p>" $username;
+   
+   $email = $_REQUEST["email"];
+   
+   echo "<p>Email Address: mailto:</p>" $email;
+   
+   $major = $_REQUEST["major"];
+   
+   echo "<p>Major: </p>" .$major;
+   
+   $comments = $_REQUEST["comments"];
+   
+   echo "<p>Comments: </p>" $comments;
 }
 
 ?>
