02b52cf9cc360300b666facd57932ac44f692dd4
moksaam <mem16001@byui.edu>
2018-01-24 22:01:32 -0700

    updated my php file

--- web/week3.php
+++ web/week3.php
@@ -1,40 +1,22 @@
-<?php
-// Variables for our form
-$username = $email = $cs = $web = $cit = $ce = $comments = "";
+<?php 
+$username = $_POST["username"];
+$email = $_POST["email"];
+$comments = $_POST["comments"];
+echo "Username: "; 
+echo $username;
+echo "<br>Email: ";
+echo "<a href='mailto:".$email."'>".$email."</a></br>";
+echo "<br>Major: ";
 
-/*if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   $username = test_input($_POST["username"]);
-   $email = test_input($_POST["email"]);
-   $major = test_input($_POST["major"]);
-   $comments = test_input($_POST["comments"]);
-}
-
-function test_input($data) {
-   $data = trim($data);
-   $data = stripslashes($data);
-   $data = htmlspecialchars($data);
-  
-   return $data;
-}
-*/
-// Check if the form has been submitted
 if (isset($_POST['submit'])) {
-   
-   $username = $_REQUEST["username"];
-   
-   echo "<p>Username: </p>" $username;
-   
-   $email = $_REQUEST["email"];
-   
-   echo "<p>Email Address: mailto:</p>" $email;
-   
-   $major = $_REQUEST["major"];
-   
-   echo "<p>Major: </p>" .$major;
-   
-   $comments = $_REQUEST["comments"];
-   
-   echo "<p>Comments: </p>" $comments;
+   if (isset($_POST['major'])) {
+         
+      $selected_major = $_POST['major'];
+      echo $selected_major;
+   }
 }
+
+echo "<br><br>Comments: ";
+echo $comments;
 
 ?>
