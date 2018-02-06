afc073872591b21a31b0dcbd6e19d28725c66f84
moksaam <mem16001@byui.edu>
2018-02-05 23:06:03 -0700

    add

--- web/Week3/cart.php
+++ web/Week3/cart.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 <?php session_start(); ?>
-=======
-<? session_start(); ?>
->>>>>>> 7f8e9a9d84f1450a20e583b314b609c012e3f7ca
 <!DOCTYPE html>
 <html>
    <head>
@@ -11,20 +7,10 @@
    </head>
    <body>
       <h1>Cart</h1>
-<<<<<<< HEAD
 
       <?php
       require("header.php");
       /*
-=======
-      <div id="items">
-         <ul class="menu">
-            <li><a href="prove03.php">Browse</a></li>
-            <li><a href="checkout.html">Checkout</a></li>
-         </ul>
-      </div> 
-      <?php
->>>>>>> 7f8e9a9d84f1450a20e583b314b609c012e3f7ca
       if (isset($_SESSION['iQ'])) {
          // Added stuff
          print_r($_SESSION['itemCost']);
@@ -33,12 +19,8 @@
          $_SESSION['iQ'] = $qnty;
          echo ('<br />The ' . $qnty . ' submit button was pressed<br />');
 }
-<<<<<<< HEAD
 */
 ?>
-=======
-?> 
->>>>>>> 7f8e9a9d84f1450a20e583b314b609c012e3f7ca
       <br><br>
       <br><br>
       <table>
@@ -50,7 +32,6 @@
          </tr>
          <?php
          $total = 0;
-<<<<<<< HEAD
 
          if (isset($_SESSION["error"])) {
            echo $_SESSION["error"] . '<br>';
@@ -69,28 +50,11 @@
                     '<button type="submit">Update</button>' .
                     '</form></td>';
 
-=======
-         
-         foreach ($_SESSION['itemQty'] as $itemID => $value) {
-            if ($value != 0) {            
-               echo("<tr>");
-               echo("<td>".$_SESSION['itemName'][$itemID]."</td>");
-               echo("<td>".$_SESSION['itemQty'][$itemID]."</td>");
-               echo("<td>".$_SESSION['itemCost'][$itemID]."</td>");
-               
->>>>>>> 7f8e9a9d84f1450a20e583b314b609c012e3f7ca
                echo("</tr>");
             }
          }
          ?>
-<<<<<<< HEAD
 
       </table>
    </body>
 </html>
-=======
-           
-      </table>
-   </body>
-</html>
->>>>>>> 7f8e9a9d84f1450a20e583b314b609c012e3f7ca
--- web/Week3/prove03.php
+++ web/Week3/prove03.php
@@ -2,7 +2,6 @@
 // Start the session
 session_start();
 ?>
-<<<<<<< HEAD
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -45,71 +44,6 @@ session_start();
   var_dump($_SESSION['itemQty']);
 }
 ?>
-=======
-<!DOCTYPE HTML>  
-<html>
-<head>
-      <title>03 Prove Assignment: PHP Shopping Cart</title>
-      <link rel="stylesheet" type="text/css" href="03prove.css">
-</head>
-<body>
-<?php
-
-   if (!isset($_SESSION['itemCost'])) {
-      $itemName = array('apple' => 'Apple', 'banana' => 'Banana', 'orange' => 'Orange', 'peach' => 'Peach');
-      $itemCost = array('apple' => 2.00, 'banana' => 3.00, 'orange' => 2.25, 'peach' => 2.50);
-      $itemQty = array('apple' => 0, 'banana' => 0, 'orange' => 0, 'peach' => 0);
-      $_SESSION['itemName'] = $itemName;
-      $_SESSION['itemCost'] = $itemCost;
-      $_SESSION['itemQty'] = $itemQty;
-   }
-   
-   if (!isset($_SESSION['itemName'])) {
-      $itemName = array('apple' => 'Apple', 'banana' => 'Banana', 'orange' => 'Orange', 'peach' => 'Peach');
-      $_SESSION['itemName'] = $itemName;
-   }
-   
-   if (!isset($_SESSION['itemCost'])) {
-      $itemCost = array('apple' => 2.00, 'banana' => 3.00, 'orange' => 2.25, 'peach' => 2.50);
-      $_SESSION['itemCost'] = $itemCost;
-   }
-   
-   if (!isset($_SESSION['itemQty'])) {
-      $itemQty = array('apple' => 0, 'banana' => 0, 'orange' => 0, 'peach' => 0);
-      $_SESSION['itemQty'] = $itemQty;
-   }
-   
-   $totalQty = 0;
-
-      $appleQty = $bananaQty = $orangeQty = $peachQty = '';
-      if (isset($_POST['action'])) {         
-         
-         switch (isset($_POST)) {
-            case isset($_POST['apple']):
-            $appleQty = $_POST['apple'];
-            break;
-            
-            case isset($_POST['banana']):
-            $bananaQty = $_POST['banana'];
-            break;
-            
-            case isset($_POST['orange']):
-            $orangeQty = $_POST['orange'];
-            break;
-            
-            case isset($_POST['peach']):
-            $peachQty = $_POST['peach'];
-            break;
-            
-            default:
-               echo ("Nothing added to cart.");
-         }
-         
-         $_SESSION['iQ'] = compact($appleQty, $bananaQty, $orangeQty, $peachQty);
-         print_r ('<br />The ' . $_SESSION['iQ'] . ' submit button was pressed<br />');
-}
-?>  
->>>>>>> 7f8e9a9d84f1450a20e583b314b609c012e3f7ca
 <h1>Items</h1>
 
 <div id="items">
@@ -120,7 +54,6 @@ session_start();
 <br><br><br><br>
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post"><input type="hidden" name="action" value="submit"/>
    <div class="main-container">
-<<<<<<< HEAD
 
       <div class="item-container">
          <img src="apple.jpg" alt="Apple">Price: $2.00<br>
@@ -154,38 +87,3 @@ session_start();
 </form>
 </body>
 </html>
-=======
-   
-      <div class="item-container">
-         <img src="apple.jpg" alt="Apple">Price: $2.00<br>
-         <span>A delicious red apple.</span><br>
-         <label for="apple">Quantity: 
-         <input type="text" name="apple" size="2" maxlength="2" value=""><br>
-         <input id="addAppleToCart" class="addToCart" type="submit" name="submit" value="Add to Cart!"></div>
-         
-      <div class="item-container">
-         <img src="banana.jpg" alt="Banana">Price: $3.00<br>
-         <span>A beautiful bunch of bananas.</span><br>
-         <label for="apple">Quantity: 
-         <input type="text" name="banana" size="2" maxlength="2" value=""><br>
-         <input id="addBananaToCart" class="addToCart" type="submit" name="submit" value="Add to Cart!"></div>
-         
-      <div class="item-container">
-         <img src="orange.jpg" alt="Orange">Price $2.25<br>
-         <span>A perfectly rounded orange.</span><br>
-         <label for="apple">Quantity: 
-         <input type="text" name="orange" size="2" maxlength="2" value=""><br>
-         <input id="addOrangeToCart" class="addToCart" type="submit" name="submit" value="Add to Cart!"></div>
-         
-      <div class="item-container">
-         <img src="peach.jpg" alt="Peach">Price: $2.50<br>
-         <span>A most desirable peach.</span><br>
-         <label for="apple">Quantity: 
-         <input type="text" name="peach" size="2" maxlength="2" value=""><br>
-         <input id="addPeachToCart" class="addToCart" type="submit" name="submit" value="Add to Cart!"></div>
-       
-   </div>
-</form>
-</body>
-</html>
->>>>>>> 7f8e9a9d84f1450a20e583b314b609c012e3f7ca
