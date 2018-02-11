8f831582796ad9621f45c72d2101a61841ab0b47
moksaam <mem16001@byui.edu>
2018-02-10 21:44:55 -0700

    add

--- web/database/cs313DB.sql
+++ web/database/cs313DB.sql
@@ -1,34 +1,35 @@
-CREATE TABLE login (
+CREATE SCHEMA phpDB;
+CREATE TABLE phpDB.login (
   id serial NOT NULL UNIQUE PRIMARY KEY,
   username varchar (50) NOT NULL,
-  creation_date date
+  creation_date date default (now())
 );
-CREATE TABLE movies (
+CREATE TABLE phpDB.movies (
   id serial NOT NULL UNIQUE PRIMARY KEY,
   title text,
   director varchar (50),
   genre varchar (50),
   year_published INT,
-  creation_date date
+  creation_date date default (now())
 );
-CREATE TABLE books (
+CREATE TABLE phpDB.books (
   id serial NOT NULL UNIQUE PRIMARY KEY,
   title text,
   author varchar (50) NOT NULL,
   genre varchar (50) NOT NULL,
-  year_published date,
-  creation_date date
+  year_published INT,
+  creation_date date default (now())
 );
-CREATE TABLE music (
+CREATE TABLE phpDB.music (
   id serial NOT NULL UNIQUE PRIMARY KEY,
   title text,
   artist varchar (50) NOT NULL,
   genre varchar (50) NOT NULL,
   year_published INT,
-  creation_date date
+  creation_date date default (now())
 );
-CREATE TABLE tables_list (
+CREATE TABLE phpDB.tables_list (
   id serial NOT NULL UNIQUE PRIMARY KEY,
   table_name varchar (50) NOT NULL,
-  creation_date date
+  creation_date date default (now())
 );
