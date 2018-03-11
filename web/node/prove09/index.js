65a6f02b8c300be1ea93a7bad77d7a759a9bc592
moksaam <mem16001@byui.edu>
2018-03-10 20:19:46 -0700

    add

--- web/node/prove09/index.js
+++ web/node/prove09/index.js
@@ -20,7 +20,7 @@ app.listen(app.get('port'), function() {
     console.log('Server listening on port ' + app.get('port'));
 });
 
-app.get('/getRate', function(response, request) {
+app.get('/getRate', function(request, response) {
     calculatePostalRate(request, response);
 });
 
