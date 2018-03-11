e2cdd0a6387c262372703e1c72c77a589756d586
moksaam <mem16001@byui.edu>
2018-03-10 19:39:38 -0700

    added npm

--- web/node/prove09/routes/index.js
+++ web/node/prove09/routes/index.js
@@ -16,7 +16,7 @@ app.get('/', function(request, response) {
     response.render('pages/index');
 });
 
-app.listen(port, function() {
+app.listen(app.get('port'), function() {
     console.log('Server listening on port ' + app.get('port'));
 });
 
